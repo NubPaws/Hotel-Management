8f68ec042544eb6a220bb768543f9d68875c63a5
NubPaws <DXroomram@gmail.com>
2024-11-19 19:01:39 +0200

    Started the creation of the room type screen

--- frontend/src/Rooms/RoomTypesScreen.tsx
+++ frontend/src/Rooms/RoomTypesScreen.tsx
@@ -1,18 +1,59 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
 import { NavigationBar } from "../UIElements/NavigationBar";
 import CenteredLabel from "../UIElements/CenteredLabel";
 import { ScreenProps } from "../Utils/Props";
 import useUserRedirect from "../Utils/Hooks/useUserRedirect";
+import useFetchRoomTypes from "./Hooks/useFetchRoomTypes";
+import FormContainer from "../UIElements/Forms/FormContainer";
+import Input, { InputType } from "../UIElements/Forms/Input";
+import SearchableDropdown from "../UIElements/Forms/SearchableDropdown";
 
 const RoomTypesScreen: FC<ScreenProps> = ({
     userCredentials,
 }) => {
     useUserRedirect(userCredentials, ["Admin"], ["FrontDesk"]);
     
+    const { roomTypes, loading } = useFetchRoomTypes(userCredentials.token);
+    
+    const [roomType, setRoomType] = useState("");
+    const [description, setDescription] = useState("");
+    
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+    }
+    
+    if (loading) {
+        return <p>Loading room types, please wait...</p>
+    }
+    
     return (
     <>
         <NavigationBar />
         <CenteredLabel>Room Types Management</CenteredLabel>
+        <FormContainer onSubmit={handleSubmit}>
+            <SearchableDropdown
+                id="room-types-dropdown"
+                options={roomTypes}
+                setValue={setRoomType}
+                label="Room type"
+                required
+            />
+            <Input
+                id="room-types-description"
+                type={InputType.Text}
+                value={description}
+                placeholder="Enter room type description"
+                label="Description"
+                onChange={(e) => setDescription(e.target.value)}
+                required
+            />
+            <Input
+                id="room-types-submit-btn"
+                type={InputType.Submit}
+                value={roomTypes.includes(roomType) ? "Update" : "Create"}
+            />
+        </FormContainer>
+        
         <form id="roomTypeCreateForm" className="fieldsContainer" action="http://localhost:8000/api/Rooms/create-type/">
             {/* <Input id="roomType" className="field" type="text" name="roomType"
                 placeholder="Enter room type" errorMessageId="roomTypeErrorMessage">
